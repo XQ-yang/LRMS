9fe7aa4708a2c6abe0a2a866556e038006df5a31
小强 <3267568158@qq.com>
2021-12-27 21:56:30 +0800

    准备

---
+++ src/test/java/com/lrms/LrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
